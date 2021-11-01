b14e5a2f8be1997c48699b2c1435840485412842
ilhmabdaziz <ilhmabdaziz@yahoo.com>
2021-11-01 22:56:21 +0700

    added endpoint getAdminUserInfo for get id user

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require("express");
-
 const app = express();
+
+const { TwitterBot } = require("./twitter-bot");
 
 const PORT = process.env.PORT || 3000;
 
+const twitterBot = new TwitterBot({
+  consumer_key: "MstSVj1NhtuCtYb2OhFxzDZ8e",
+  consumer_secret: "7EcUTZRlqHo2vVcb9AjWBFb3qGebWp0eQf7p7OQHLsQP37xlbc",
+  access_token: "1351298635409944577-vL64XD1BYEywIcnNScobSb7sMjlyyd",
+  access_token_secret: "2QxpyVxZyIrlLpcM7hRGe7s0nyuPnwIvduZr8pMw0Y0Vd",
+});
+
 app.get("/", (req, res, next) => {
   res.send("Welcome to twitter bot server!");
+});
+
+app.get("/adminProfile", async (req, res, next) => {
+  const admin = await twitterBot.getAdminUserInfo();
+  res.json(admin);
 });
 
 app.listen(PORT, () => {
---
+++ twitter-bot.js
@@ -0,0 +1,27 @@
+const Twit = require("twit");
+
+class TwitterBot {
+  constructor(props) {
+    this.T = new Twit({
+      consumer_key: props.consumer_key,
+      consumer_secret: props.consumer_secret,
+      access_token: props.access_token,
+      access_token_secret: props.access_token_secret,
+    });
+  }
+
+  // Get admin user info
+  getAdminUserInfo = () => {
+    return new Promise((resolve, reject) => {
+      this.T.get("account/verify_credentials", { skip_status: true })
+        .then((result) => {
+          resolve(result);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  };
+}
+
+module.exports = { TwitterBot };
