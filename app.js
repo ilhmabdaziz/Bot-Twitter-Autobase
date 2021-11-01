7dbfdf3ec40b812ec54d5fa0aa7c6bd42f357a4c
ilhmabdaziz <ilhmabdaziz@yahoo.com>
2021-11-01 20:28:35 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res, next) => {
+  res.send("Welcome to twitter bot server backend");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening to port ${PORT}`);
+});
