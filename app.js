6110c38b45a647d58a2ff9dcbeaecf9714286d1f
ilhmabdaziz <ilhmabdaziz@yahoo.com>
2021-11-02 12:36:04 +0700

    filter received message
    :

--- app.js
+++ app.js
@@ -6,25 +6,30 @@ const { TwitterBot } = require("./twitter-bot");
 
 const PORT = process.env.PORT || 3000;
 
-const twitterBot = new TwitterBot({
+const bot = new TwitterBot({
   consumer_key: "MstSVj1NhtuCtYb2OhFxzDZ8e",
   consumer_secret: "7EcUTZRlqHo2vVcb9AjWBFb3qGebWp0eQf7p7OQHLsQP37xlbc",
   access_token: "1351298635409944577-vL64XD1BYEywIcnNScobSb7sMjlyyd",
   access_token_secret: "2QxpyVxZyIrlLpcM7hRGe7s0nyuPnwIvduZr8pMw0Y0Vd",
 });
 
-const job = new CronJob("*/2 * * * * *", doJob, null, true);
-function doJob() {
-  console.log("Ketrigger nih...!");
+const job = new CronJob("*/2 * * * * *", doJob, null, false);
+async function doJob() {
+  const authenticatedUserId = await bot.getAdminUserInfo();
+  const dm = await bot.getDirectMessage(authenticatedUserId);
+  // console.log(dm);
+  // for (const message of dm.events) {
+  //   console.log(message.message_create, "<<<< msg");
+  // }
 }
 
 app.get("/", (req, res, next) => {
   res.send("Welcome to twitter bot server!");
 });
 
-app.get("/adminProfile", async (req, res, next) => {
-  const admin = await twitterBot.getAdminUserInfo();
-  res.json(admin);
+app.get("/trigger", async (req, res, next) => {
+  job.fireOnTick();
+  res.send("job triggered!");
 });
 
 app.listen(PORT, () => {
--- twitter-bot.js
+++ twitter-bot.js
@@ -15,12 +15,38 @@ class TwitterBot {
     return new Promise((resolve, reject) => {
       this.T.get("account/verify_credentials", { skip_status: true })
         .then((result) => {
-          resolve(result);
+          const userId = result.data.id_str;
+          resolve(userId);
+          // resolve(result);
         })
         .catch((err) => {
           reject(err);
         });
     });
+  };
+
+  // Get Direct Message
+  getDirectMessage = (userId) => {
+    return new Promise((resolve, reject) => {
+      this.T.get("direct_messages/events/list", (error, data) => {
+        if (!error) {
+          console.log(userId, "USER ID  <<<<<<<");
+          const message = data.events;
+          const receivedMessage = this.getReceivedMessages(message, userId);
+
+          console.log(receivedMessage, "messages <<<<");
+
+          resolve(data);
+        } else {
+          reject("error on get direct message");
+        }
+      });
+    });
+  };
+
+  // Filter Message
+  getReceivedMessages = (messages, userId) => {
+    return messages.filter((msg) => msg.message_create.sender_id != userId);
   };
 }
 
