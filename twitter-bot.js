528e33db5e02444a6170900b1a90ef7e465e26c3
ilhmabdaziz <ilhmabdaziz@yahoo.com>
2021-11-02 19:32:41 +0700

    add method delete no trigger by id

--- twitter-bot.js
+++ twitter-bot.js
@@ -67,7 +67,7 @@ class TwitterBot {
   // Get Direct Message
   getDirectMessage = (userId) => {
     return new Promise((resolve, reject) => {
-      this.T.get("direct_messages/events/list", (error, data) => {
+      this.T.get("direct_messages/events/list", async (error, data) => {
         if (!error) {
           // console.log(userId, "USER ID  <<<<<<<");
           const messages = data.events;
@@ -80,14 +80,16 @@ class TwitterBot {
             receivedMessages,
             this.triggerWord
           );
-          console.log(
-            JSON.stringify(unnecessaryMessages, null, 3),
-            "unnes messages <<<<"
-          );
-          console.log(
-            JSON.stringify(triggerMessages, null, 3),
-            "trigger messages <<<<"
-          );
+          // console.log(
+          //   JSON.stringify(unnecessaryMessages, null, 3),
+          //   "unnes messages <<<<"
+          // );
+          // console.log(
+          //   JSON.stringify(triggerMessages, null, 3),
+          //   "trigger messages <<<<"
+          // );
+
+          await this.deleteUnnecessaryMessages(unnecessaryMessages);
 
           resolve(data);
         } else {
@@ -98,7 +100,44 @@ class TwitterBot {
   };
 
   // Method Delete no trigger
-  deleteUnnecessaryMessages = (unnecessaryMessages) => {};
+  deleteUnnecessaryMessages = async (unnecessaryMessages) => {
+    if (unnecessaryMessages.length > 3) {
+      for (let i = 0; i < 3; i++) {
+        await this.deleteMessage(unnecessaryMessages[i]);
+        await this.sleep(2000);
+      }
+    } else {
+      for (const msg of unnecessaryMessages) {
+        await this.deleteMessage(msg);
+        await this.sleep(2000);
+      }
+    }
+  };
+
+  // Delete Message
+  deleteMessage = (message) => {
+    return new Promise((resolve, reject) => {
+      this.T.delete(
+        "direct_messages/events/destroy",
+        { id: message.id },
+        (error, data) => {
+          if (!error) {
+            const msg = `Message with id: ${message.id} has been successfuly deleted`;
+            console.log(msg);
+            resolve({
+              message: msg,
+              data,
+            });
+          } else {
+            reject(error);
+          }
+        }
+      );
+    });
+  };
+
+  // Sleep 2 detik
+  sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));
 }
 
 module.exports = { TwitterBot };
